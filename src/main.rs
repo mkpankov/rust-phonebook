b82c046f4856429292925a5d66ff6acac41d5bd1
Michael Pankov <work@michaelpankov.com>
2015-06-03 19:37:00 +0300

    Add params parsing from INI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 extern crate postgres;
+extern crate ini;
 
-use postgres::{Connection, SslMode};
+use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
+use ini::Ini;
+
+use std::str::FromStr;
 
 struct Person {
     id: i32,
     name: String,
     data: Option<Vec<u8>>
+}
+
+fn params() -> (ConnectParams, SslMode) {
+    let conf = Ini::load_from_file("conf.ini").unwrap();
+
+    let host = conf.get("host").unwrap();
+    let port = conf.get("port").unwrap();
+    let sslmode = conf.get("sslmode").unwrap();
+    let dbname = conf.get("dbname").unwrap();
+    let user = conf.get("user").unwrap();
+    let pass = conf.get("pass").unwrap();
+
+    let s = match sslmode {
+        "disable" => SslMode::None,
+        "enable" => unimplemented!(),
+        _ => panic!("Wrong sslmode"),
+    };
+
+    (ConnectParams {
+        target: ConnectTarget::Tcp(host.to_owned()),
+        port: Some(FromStr::from_str(port).ok().unwrap()),
+        user: Some(UserInfo {
+            user: user.to_owned(),
+            password: Some(pass.to_owned()),
+        }),
+        database: Some(dbname.to_owned()),
+        options: vec![],
+    }, s)
 }
 
 fn main() {
