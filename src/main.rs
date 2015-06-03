d95775fd16f1c88aaa823248f05b417d7b6c6056
Michael Pankov <work@michaelpankov.com>
2015-06-03 18:40:43 +0300

    Add postgres hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate postgres;
+
+use postgres::{Connection, SslMode};
+
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::connect("postgres://postgres:postgres@localhost", &SslMode::None)
+            .unwrap();
+
+    conn.execute("CREATE TABLE person (
+                    id              SERIAL PRIMARY KEY,
+                    name            VARCHAR NOT NULL,
+                    data            BYTEA
+                  )", &[]).unwrap();
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None
+    };
+    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+                 &[&me.name, &me.data]).unwrap();
+
+    let stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
+    for row in stmt.query(&[]).unwrap() {
+        let person = Person {
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2)
+        };
+        println!("Found person {}", person.name);
+    }
 }
