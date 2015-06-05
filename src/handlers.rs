8ade10dcab417897fa564efa24d20cd201cfc530
Michael Pankov <work@michaelpankov.com>
2015-06-05 20:46:16 +0300

    Move handlers to separate module

---
+++ src/handlers.rs
@@ -0,0 +1,136 @@
+use iron::*;
+use iron::mime::{Mime, TopLevel, SubLevel};
+use postgres::Connection;
+use rustc_serialize::json;
+
+use std::io::Read;
+use std::sync::{Arc, Mutex};
+
+pub fn get_records(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
+    let url = req.url.clone().into_generic_url();
+    let mut name: Option<String> = None;
+    if let Some(qp) = url.query_pairs() {
+        for (key, value) in qp {
+            match (&key[..], value) {
+                ("name", n) => {
+                    if let None = name {
+                        name = Some(n);
+                    } else {
+                        return Ok(Response::with((status::BadRequest, "passed name in query more than once")));
+                    }
+                }
+                _ => return Ok(Response::with((status::BadRequest, "unexpected query parameters"))),
+            }
+        }
+    }
+
+    let mut json_records;
+    if let Ok(recs) = ::db::read(sdb, name.as_ref().map(|s| &s[..])) {
+        use rustc_serialize::json;
+        if let Ok(json) = json::encode(&recs) {
+            json_records = Some(json);
+        } else {
+            return Ok(Response::with((status::InternalServerError, "couldn't convert records to JSON")));
+        }
+    } else {
+        return Ok(Response::with((status::InternalServerError, "couldn't read records from database")));
+    }
+    let content_type = Mime(
+        TopLevel::Application, SubLevel::Json, Vec::new());
+
+    Ok(Response::with(
+        (content_type, status::Ok, json_records.unwrap())))
+}
+
+pub fn get_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
+    let url = req.url.clone().into_generic_url();
+    let path = url.path().unwrap();
+    let sid: &str = &path.iter().last().unwrap();
+    let id;
+    if let Ok(r) = sid.parse() {
+        id = r;
+    } else {
+        return Ok(Response::with((status::BadRequest, "bad id")));
+    }
+
+    let mut json_record;
+    if let Ok(recs) = ::db::read_one(sdb, id) {
+        if let Ok(json) = json::encode(&recs) {
+            json_record = Some(json);
+        } else {
+            return Ok(Response::with((status::InternalServerError, "couldn't convert records to JSON")));
+        }
+    } else {
+        return Ok(Response::with((status::InternalServerError, "couldn't read records from database")));
+    }
+    let content_type = Mime(
+        TopLevel::Application, SubLevel::Json, Vec::new());
+
+    Ok(Response::with(
+        (content_type, status::Ok, json_record.unwrap())))
+}
+
+pub fn add_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
+    let mut body = String::new();
+    req.body.read_to_string(&mut body).unwrap();
+    let decoded: json::DecodeResult<::db::Record> = json::decode(&body);
+    if let Ok(record) = decoded {
+        if record.name == "" || record.phone == "" {
+            return Ok(Response::with((status::BadRequest, "empty name or phone")))
+        }
+        if let Ok(_) = ::db::insert(&*sdb.lock().unwrap(), &record.name, &record.phone) {
+            Ok(Response::with((status::Created)))
+        } else {
+            Ok(Response::with((status::InternalServerError, "couldn't insert record")))
+        }
+    } else {
+        return Ok(Response::with((status::BadRequest, "couldn't decode JSON")));
+    }
+}
+
+pub fn update_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
+    let url = req.url.clone().into_generic_url();
+    let path = url.path().unwrap();
+    let sid: &str = &path.iter().last().unwrap();
+    let id;
+    if let Ok(r) = sid.parse() {
+        id = r;
+    } else {
+        return Ok(Response::with((status::BadRequest, "bad id")));
+    }
+
+    let mut body = String::new();
+    req.body.read_to_string(&mut body).unwrap();
+    let decoded: json::DecodeResult<::db::Record> = json::decode(&body);
+    if let Ok(record) = decoded {
+        if record.name == "" || record.phone == "" {
+            return Ok(Response::with((status::BadRequest, "empty name or phone")))
+        }
+        if let Ok(_) = ::db::update(&*sdb.lock().unwrap(), id, &record.name, &record.phone) {
+            Ok(Response::with((status::NoContent)))
+        } else {
+            Ok(Response::with((status::NotFound, "couldn't update record")))
+        }
+    } else {
+        return Ok(Response::with((status::BadRequest, "couldn't decode JSON")));
+    }
+}
+
+
+pub fn delete_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
+    let url = req.url.clone().into_generic_url();
+    let path = url.path().unwrap();
+    let sid: &str = &path.iter().last().unwrap();
+    let id;
+    if let Ok(r) = sid.parse() {
+        id = r;
+    } else {
+        return Ok(Response::with((status::BadRequest, "bad id")));
+    }
+
+    if let Ok(_) = ::db::remove(&*sdb.lock().unwrap(), &[id]) {
+        Ok(Response::with((status::NoContent)))
+    } else {
+        Ok(Response::with((status::NotFound, "couldn't update record")))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ extern crate url;
 
 use ini::Ini;
 use iron::*;
-use iron::mime::{Mime, TopLevel, SubLevel};
 use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
-use rustc_serialize::json;
 
-use std::io::Read;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
 mod db;
+mod handlers;
 
 const HELP: &'static str = "Usage: phonebook COMMAND [ARG]...
 Commands:
@@ -121,31 +119,31 @@ fn main() {
                         let sdb_ = sdb.clone();
                         router.get("/api/v1/records",
                                    move |req: &mut Request|
-                                   get_records(sdb_.clone(), req));
+                                   handlers::get_records(sdb_.clone(), req));
                     }
                     {
                         let sdb_ = sdb.clone();
                         router.get("/api/v1/records/:id",
                                    move |req: &mut Request|
-                                   get_record(sdb_.clone(), req));
+                                   handlers::get_record(sdb_.clone(), req));
                     }
                     {
                         let sdb_ = sdb.clone();
                         router.post("/api/v1/records",
                                 move |req: &mut Request|
-                                add_record(sdb_.clone(), req));
+                                handlers::add_record(sdb_.clone(), req));
                     }
                     {
                         let sdb_ = sdb.clone();
                         router.put("/api/v1/records/:id",
                                    move |req: &mut Request|
-                                   update_record(sdb_.clone(), req));
+                                   handlers::update_record(sdb_.clone(), req));
                     }
                     {
                         let sdb_ = sdb.clone();
                         router.delete("/api/v1/records/:id",
                                       move |req: &mut Request|
-                                      delete_record(sdb_.clone(), req));
+                                      handlers::delete_record(sdb_.clone(), req));
 
                     }
 
@@ -156,134 +154,5 @@ fn main() {
             }
         }
         None => panic!("No command supplied"),
-    }
-}
-
-fn get_records(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
-    let url = req.url.clone().into_generic_url();
-    let mut name: Option<String> = None;
-    if let Some(qp) = url.query_pairs() {
-        for (key, value) in qp {
-            match (&key[..], value) {
-                ("name", n) => {
-                    if let None = name {
-                        name = Some(n);
-                    } else {
-                        return Ok(Response::with((status::BadRequest, "passed name in query more than once")));
-                    }
-                }
-                _ => return Ok(Response::with((status::BadRequest, "unexpected query parameters"))),
-            }
-        }
-    }
-
-    let mut json_records;
-    if let Ok(recs) = db::read(sdb, name.as_ref().map(|s| &s[..])) {
-        use rustc_serialize::json;
-        if let Ok(json) = json::encode(&recs) {
-            json_records = Some(json);
-        } else {
-            return Ok(Response::with((status::InternalServerError, "couldn't convert records to JSON")));
-        }
-    } else {
-        return Ok(Response::with((status::InternalServerError, "couldn't read records from database")));
-    }
-    let content_type = Mime(
-        TopLevel::Application, SubLevel::Json, Vec::new());
-
-    Ok(Response::with(
-        (content_type, status::Ok, json_records.unwrap())))
-}
-
-fn get_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
-    let url = req.url.clone().into_generic_url();
-    let path = url.path().unwrap();
-    let sid: &str = &path.iter().last().unwrap();
-    let id;
-    if let Ok(r) = sid.parse() {
-        id = r;
-    } else {
-        return Ok(Response::with((status::BadRequest, "bad id")));
-    }
-
-    let mut json_record;
-    if let Ok(recs) = db::read_one(sdb, id) {
-        if let Ok(json) = json::encode(&recs) {
-            json_record = Some(json);
-        } else {
-            return Ok(Response::with((status::InternalServerError, "couldn't convert records to JSON")));
-        }
-    } else {
-        return Ok(Response::with((status::InternalServerError, "couldn't read records from database")));
-    }
-    let content_type = Mime(
-        TopLevel::Application, SubLevel::Json, Vec::new());
-
-    Ok(Response::with(
-        (content_type, status::Ok, json_record.unwrap())))
-}
-
-fn add_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
-    let mut body = String::new();
-    req.body.read_to_string(&mut body).unwrap();
-    let decoded: json::DecodeResult<db::Record> = json::decode(&body);
-    if let Ok(record) = decoded {
-        if record.name == "" || record.phone == "" {
-            return Ok(Response::with((status::BadRequest, "empty name or phone")))
-        }
-        if let Ok(_) = db::insert(&*sdb.lock().unwrap(), &record.name, &record.phone) {
-            Ok(Response::with((status::Created)))
-        } else {
-            Ok(Response::with((status::InternalServerError, "couldn't insert record")))
-        }
-    } else {
-        return Ok(Response::with((status::BadRequest, "couldn't decode JSON")));
-    }
-}
-
-fn update_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
-    let url = req.url.clone().into_generic_url();
-    let path = url.path().unwrap();
-    let sid: &str = &path.iter().last().unwrap();
-    let id;
-    if let Ok(r) = sid.parse() {
-        id = r;
-    } else {
-        return Ok(Response::with((status::BadRequest, "bad id")));
-    }
-
-    let mut body = String::new();
-    req.body.read_to_string(&mut body).unwrap();
-    let decoded: json::DecodeResult<db::Record> = json::decode(&body);
-    if let Ok(record) = decoded {
-        if record.name == "" || record.phone == "" {
-            return Ok(Response::with((status::BadRequest, "empty name or phone")))
-        }
-        if let Ok(_) = db::update(&*sdb.lock().unwrap(), id, &record.name, &record.phone) {
-            Ok(Response::with((status::NoContent)))
-        } else {
-            Ok(Response::with((status::NotFound, "couldn't update record")))
-        }
-    } else {
-        return Ok(Response::with((status::BadRequest, "couldn't decode JSON")));
-    }
-}
-
-
-fn delete_record(sdb: Arc<Mutex<Connection>>, req: &mut Request) -> IronResult<Response> {
-    let url = req.url.clone().into_generic_url();
-    let path = url.path().unwrap();
-    let sid: &str = &path.iter().last().unwrap();
-    let id;
-    if let Ok(r) = sid.parse() {
-        id = r;
-    } else {
-        return Ok(Response::with((status::BadRequest, "bad id")));
-    }
-
-    if let Ok(_) = db::remove(&*sdb.lock().unwrap(), &[id]) {
-        Ok(Response::with((status::NoContent)))
-    } else {
-        Ok(Response::with((status::NotFound, "couldn't update record")))
     }
 }
